c97f17accdd293d8091cdc894bb64ed51dee26aa
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2023-03-16 21:49:00 +0100

    Cambio de idioma!

--- app/src/main/java/com/cristobalbernal/lacasanostraapk/MainActivity.java
+++ app/src/main/java/com/cristobalbernal/lacasanostraapk/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -53,9 +54,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getMenu().getItem(0).setChecked(true);
-
-
-
     }
 
     @Override
--- app/src/main/java/com/cristobalbernal/lacasanostraapk/fragments/Fragment_Home.java
+++ app/src/main/java/com/cristobalbernal/lacasanostraapk/fragments/Fragment_Home.java
@@ -1,11 +1,35 @@
 package com.cristobalbernal.lacasanostraapk.fragments;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.cristobalbernal.lacasanostraapk.R;
 
 public class Fragment_Home extends Fragment {
     public Fragment_Home(){
         super(R.layout.fragment_home);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button button = view.findViewById(R.id.btCartaHome);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getParentFragmentManager();
+                manager.beginTransaction()
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .replace(R.id.content_frame, Fragment_Carta.class, null)
+                        .commit();
+            }
+        });
     }
 }
