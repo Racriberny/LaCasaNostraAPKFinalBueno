de29860780bb916412c52676d77d2a302e0b8088
Cristobal <cristobaljunior9@gmail.com>
2023-03-24 20:55:10 +0100

    hacer esta noche login!!

--- app/src/main/java/com/cristobalbernal/lacasanostraapk/fragments/Fragment_Acceder.java
+++ app/src/main/java/com/cristobalbernal/lacasanostraapk/fragments/Fragment_Acceder.java
@@ -3,6 +3,7 @@ package com.cristobalbernal.lacasanostraapk.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,7 @@ import com.cristobalbernal.lacasanostraapk.R;
 
 
 public class Fragment_Acceder extends Fragment {
+
     public Fragment_Acceder(){
         super(R.layout.fragment_acceder);
     }
@@ -22,14 +24,24 @@ public class Fragment_Acceder extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Button login = view.findViewById(R.id.btnLogin);
 
-
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLogin();
+            }
+        });
 
         view.findViewById(R.id.tvRegistrar).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(),"Hola",Toast.LENGTH_LONG).show();
+
             }
         });
+    }
+
+    private void getLogin() {
+
     }
 }
