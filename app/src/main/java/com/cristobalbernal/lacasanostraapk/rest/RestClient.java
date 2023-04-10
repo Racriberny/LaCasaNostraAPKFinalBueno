40bd56ebb760c81226ff76a16192af322f840390
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2023-04-10 18:01:37 +0200

    mirar lo de los iconos!!

--- app/src/main/java/com/cristobalbernal/lacasanostraapk/rest/RestClient.java
+++ app/src/main/java/com/cristobalbernal/lacasanostraapk/rest/RestClient.java
@@ -7,9 +7,9 @@ public class RestClient {
     private static IAPIService instance;
     public static final int PORT = 8080;
 
-    public static final String CASA = "192.168.189.181";
-    public static final String INSTI = "192.168.20.148";
-    private static final String BASE_URL = "http://" + CASA + ":" +  PORT + "/";
+    public static final String CASA = "192.168.1.44";
+    public static final String INSTI = "192.168.189.181";
+    private static final String BASE_URL = "http://" + INSTI + ":" +  PORT + "/";
 
     /* Lo hacemos privado para evitar que se puedan crear instancias de esta forma */
     private RestClient() {
