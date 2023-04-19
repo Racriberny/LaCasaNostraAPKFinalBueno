fe3397b8614932be422e0b54e69aead75e56ba73
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2023-04-19 13:25:36 +0200

    Update EncodingImg.java
    Método para pasar de imagen Uri a base64

--- app/src/main/java/com/cristobalbernal/lacasanostraapk/Utils/EncodingImg.java
+++ app/src/main/java/com/cristobalbernal/lacasanostraapk/Utils/EncodingImg.java
@@ -10,4 +10,18 @@ public class EncodingImg {
         byte[] bytes = Base64.decode(sImage,Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(bytes,0,bytes.length);
     }
+    //Pasar la imagen de Uri a Base64
+    private String getBase64FromUri(Uri uri) throws IOException {
+        @SuppressLint("Recycle") InputStream inputStream = getContentResolver().openInputStream(uri);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        byte[] buffer = new byte[100000];
+        int length;
+        while ((length = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, length);
+        }
+
+        byte[] imageBytes = outputStream.toByteArray();
+        return Base64.encodeToString(imageBytes, Base64.DEFAULT);
+    }
 }
